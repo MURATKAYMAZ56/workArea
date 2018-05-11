c65bfc31fd7858521af062e09d92c7e68efd9c4c
MURATKAYMAZ56 <muratkaymaz56@gmail.com>
2018-05-11 19:58:21 +0200

    lynda

---
+++ lynda.com/NODEwith LYNDA/Ch03/03_01/start/global.js
@@ -0,0 +1,13 @@
+
+const path = require('path');
+
+
+//let hello = "HEllo WOrld From Node";
+
+//let justNode = hello.slice(17);
+//console.log(`Rock on World from ${justNode}`);
+
+//console.log(__dirname);
+//console.log(__filename);
+
+console.log(`Rock on World from ${__filename}`)
