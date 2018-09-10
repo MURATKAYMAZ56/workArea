c7d18219d26a99ce2290b431a01147301540108f
MURATKAYMAZ56 <muratkaymaz56@gmail.com>
2018-09-10 23:19:40 +0200

    keppp worlking on react

--- WESBOS React/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/index.js
+++ WESBOS React/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/index.js
@@ -1,2 +1,10 @@
-// let's go!
-alert("hopp")
+import React from 'react';
+import {
+    render
+} from 'react';
+class StorePicker extends React.Component {
+    render() {
+        return <p > Hello < /p>
+    }
+}
+render( < StorePicker / > , document.querySelector('#main'));
